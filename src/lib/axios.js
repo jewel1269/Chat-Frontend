1e774cb0a7e47b47946c82e03a782e11acf13897
Jewel Mia <145703213+jewel1269@users.noreply.github.com>
2025-03-07 17:22:59 +0600

    add new dist file

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:"http://localhost:5000/api",
+  baseURL:"https://chat-backend-red.vercel.app/api",
   withCredentials: true,
 });
 
